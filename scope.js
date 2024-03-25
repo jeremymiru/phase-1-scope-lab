1d08d842839d16974315dbbed84a9bee6fd1ec78
jeremymiru <mirujeremy@gmail.com>
2024-03-25 21:26:56 +0300

    scope.js

---
+++ scope.js
@@ -0,0 +1,16 @@
+var customerName = "bob"
+const leastFavoriteCustomer = "amber"
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer () {
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'victor';
+}
